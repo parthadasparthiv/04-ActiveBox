7eb61bae4336688c7b68f5d2357fb6c013bcc890
Partha Das <133395896+parthadasparthiv@users.noreply.github.com>
2024-06-07 11:21:37 +0600

    04-ActiveBox
    ActiveBox_Responsive_web-site

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  // var swiper = new Swiper(".mySwiper", {
+  //   slidesPerView: "auto",
+  //   centeredSlides: true,
+  //   spaceBetween: 30,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  // });
